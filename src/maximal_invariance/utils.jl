2a2dd5ee646a4eddfa3c2e03352dafc98ea42248
ueliwechsler <ueli.wechsler@outlook.com>
2023-04-30 12:11:42 +0200

    CI/CD for registering the package (#10)
    * refactor: add workflow files and bring code up-to-date
    
    * fix: ci.myl run doctests

--- src/maximal_invariance/utils.jl
+++ src/maximal_invariance/utils.jl
@@ -1,21 +1,25 @@
+import Polyhedra
+import CDDLib
 
-function _projection(projection_idx, E::AbstractMatrix{N}, e::AbstractVector{N},
-                             backend=nothing,
-                             algorithm=nothing,
-                             prune=true) where {N<:Real}
-    require(:Polyhedra; fun_name="_projection")
-    require(:CDDLib; fun_name="_projection")
-    if backend == nothing
+function _projection(
+    projection_idx,
+    E::AbstractMatrix{N},
+    e::AbstractVector{N},
+    backend=nothing,
+    algorithm=nothing,
+    prune=true
+) where {N<:Real}
+    if isnothing(backend)
         backend = LazySets.default_cddlib_backend(N)
     end
 
-    if algorithm == nothing
+    if isnothing(algorithm)
         algorithm = Polyhedra.FourierMotzkin()
     elseif !(algorithm <: EliminationAlgorithm)
         error("the algorithm $algorithm is not a valid elimination algorithm;
               choose among any of $(subtypes(Polyhedra.EliminationAlgorithm))")
     end
-    eliminiation_idx = setdiff(1:size(E,2), projection_idx)
+    eliminiation_idx = setdiff(1:size(E, 2), projection_idx)
     PQ = HPolyhedron(E, e)
     PQ_cdd = polyhedron(PQ, backend=backend)
     W = HPolyhedron(Polyhedra.eliminate(PQ_cdd, eliminiation_idx, algorithm))
--- test/helper/jump.jl
+++ test/helper/jump.jl
@@ -5,7 +5,7 @@ using LazySets
 using JuMP
 
 @testset "add_constraint! as Vector to x[1:n]" begin
-    n=3;
+    n = 3
     x0 = 1:n
     mod = Model()
     @variable(mod, x[1:n])
@@ -13,29 +13,30 @@ using JuMP
     con1 = all_constraints(mod, AffExpr, MOI.EqualTo{Float64})
     m2 = Model()
     @variable(m2, x[1:n])
-    @constraint(m2, x[:,1] .== x0)
+    @constraint(m2, x[:, 1] .== x0)
     con2 = all_constraints(m2, AffExpr, MOI.EqualTo{Float64})
     @test getfield.(con1, :index) == getfield.(con2, :index)
 end
 
 @testset "add_constraint! as Vector to x[1:n,1:m]" begin
-    n=3; m=2
+    n = 3
+    m = 2
     x0 = 1:n
     mod = Model()
-    @variable(mod, x[1:n,1:m])
+    @variable(mod, x[1:n, 1:m])
     InvariantSets.add_constraint!(mod, x, x0)
     con1 = all_constraints(mod, AffExpr, MOI.EqualTo{Float64})
     m2 = Model()
-    @variable(m2, x[1:n,1:m])
-    @constraint(m2, x[:,1] .== x0)
-    @constraint(m2, x[:,2] .== x0)
+    @variable(m2, x[1:n, 1:m])
+    @constraint(m2, x[:, 1] .== x0)
+    @constraint(m2, x[:, 2] .== x0)
     con2 = all_constraints(m2, AffExpr, MOI.EqualTo{Float64})
     @test getfield.(con1, :index) == getfield.(con2, :index)
 end
 
 @testset "add_constraint! as Polytope to  x[1:n]" begin
-    F =  [1 0 0; 0 1 0; 0 0 1]
-    g = [1, 1, 1]
+    F = [1 0 0; 0.0 1 0; 0 0 1]
+    g = [1, 1, 1.0]
     A = HPolytope(F, g)
     m = Model()
     @variable(m, x[1:3])
@@ -43,24 +44,25 @@ end
     con1 = all_constraints(m, AffExpr, MOI.LessThan{Float64})
     m2 = Model()
     @variable(m2, x[1:3])
-    @constraint(m2, F*x .<= g)
+    @constraint(m2, F * x .<= g)
     con2 = all_constraints(m2, AffExpr, MOI.LessThan{Float64})
     @test getfield.(con1, :index) == getfield.(con2, :index)
 end
 
 @testset "add_constraint! as Vector to x[1:n,1:m]" begin
-    n=3; m=2
-    F =  [1 0 0; 0 1 0; 0 0 1]
-    g = [1, 1, 1]
+    n = 3
+    m = 2
+    F = [1 0 0; 0 1 0; 0 0 1.0]
+    g = [1, 1, 1.0]
     A = HPolytope(F, g)
     mod = Model()
-    @variable(mod, x[1:n,1:m])
+    @variable(mod, x[1:n, 1:m])
     InvariantSets.add_constraint!(mod, x, A)
     con1 = all_constraints(mod, AffExpr, MOI.LessThan{Float64})
     m2 = Model()
-    @variable(m2, x[1:n,1:m])
-    @constraint(m2, F*x[:,1] .<= g)
-    @constraint(m2, F*x[:,2] .<= g)
+    @variable(m2, x[1:n, 1:m])
+    @constraint(m2, F * x[:, 1] .<= g)
+    @constraint(m2, F * x[:, 2] .<= g)
     con2 = all_constraints(m2, AffExpr, MOI.LessThan{Float64})
     @test getfield.(con1, :index) == getfield.(con2, :index)
 end
@@ -75,7 +77,7 @@ end
     m2 = Model()
     @variable(m2, x[1:3])
     F, g = tosimplehrep(A)
-    @constraint(m2, F*x .<= g)
+    @constraint(m2, F * x .<= g)
     con2 = all_constraints(m2, AffExpr, MOI.LessThan{Float64})
     @test getfield.(con1, :index) == getfield.(con2, :index)
 end
